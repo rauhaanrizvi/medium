28ebedaa6420bad7d1d011e576d926619aeb8bb4
Harum Naseem <harum.naseem55@gmail.com>
2021-03-29 09:56:38 +0500

    -

---
+++ eslintrc.js
@@ -0,0 +1,14 @@
+if (hasESLint)
+{
+  module.exports = {
+    plugins: ['wdio'],
+    extends: 'plugin:wdio/recommended',
+    env: {
+      mocha: true
+    },
+    rules: {
+      strict: 'off'
+    }
+  }
+}
+
